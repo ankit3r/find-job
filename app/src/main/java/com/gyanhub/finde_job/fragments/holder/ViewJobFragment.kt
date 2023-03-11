8382c746d3573a1e5578ca605fa4229c2c697af3
ankit3r <64691445+ankit3r@users.noreply.github.com>
2023-03-11 22:50:00 +0530

    applied successful

--- app/src/main/java/com/gyanhub/finde_job/fragments/holder/ViewJobFragment.kt
+++ app/src/main/java/com/gyanhub/finde_job/fragments/holder/ViewJobFragment.kt
@@ -1,28 +1,46 @@
 package com.gyanhub.finde_job.fragments.holder
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.AlertDialog
+import android.app.DownloadManager
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import android.os.Environment
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.content.FileProvider
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.google.common.reflect.TypeToken
-import com.google.gson.Gson
 import com.gyanhub.finde_job.R
 import com.gyanhub.finde_job.databinding.FragmentViewJobBinding
 import com.gyanhub.finde_job.databinding.LayoutApplyBinding
-import com.gyanhub.finde_job.databinding.PostJobBottomBinding
-import com.gyanhub.finde_job.model.State
+import com.gyanhub.finde_job.viewModle.AuthViewModel
 import com.gyanhub.finde_job.viewModle.DbViewModel
+import okhttp3.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
+@SuppressLint("SetTextI18n")
 class ViewJobFragment(private val jobId: String) : Fragment() {
     private lateinit var binding: FragmentViewJobBinding
     private lateinit var bottomBinding: LayoutApplyBinding
     private lateinit var dbModel: DbViewModel
+    private lateinit var userDb: AuthViewModel
     private lateinit var dialog: AlertDialog
+    private lateinit var hrNo: String
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,21 +48,23 @@ class ViewJobFragment(private val jobId: String) : Fragment() {
     ): View {
         binding = FragmentViewJobBinding.inflate(layoutInflater, container, false)
         dbModel = ViewModelProvider(this)[DbViewModel::class.java]
+        userDb = ViewModelProvider(this)[AuthViewModel::class.java]
         progressBar()
-        dbModel.getJobById(jobId) { success, data, error ->
+        hrNo = ""
+        dbModel.getJobById(jobId) { success, data, _ ->
             if (success && data != null) {
                 data.apply {
                     binding.apply {
-                        txtJobTitle.text = jobTitle
-                        txtJobType.text = jobType
+                        txtJobTitle.text = "$jobTitle ($jobType)"
                         txtJobDis.text = jobDisc
-                        txtPay.text = pay + " /Month"
-                        txtCyName.text = jobCyName
+                        txtAbout.text = "About $jobType"
+                        txtPay.text = "$pay /Month"
+                        txtCyName.text = "$jobCyName ($city, $state)"
                         txtSkill.text = skils.toString()
-                        txtOpportunities.text = jobPostOpportunitity
+                        txtOpportunities.text = "Numbers of Vacancies :- $jobPostOpportunitity"
                         txtWhoCanApply.text = whoCanApply
-                        txtlocation.text = city + ", " + state
                     }
+                    hrNo = whNo
                 }
                 dialog.dismiss()
 
@@ -68,11 +88,38 @@ class ViewJobFragment(private val jobId: String) : Fragment() {
         dialog = builder.create()
         dialog.show()
     }
+
+
+    @SuppressLint("QueryPermissionsNeeded")
     private fun bottomSheet() {
-       val bottomSheetDialog = BottomSheetDialog(requireActivity())
+        val bottomSheetDialog = BottomSheetDialog(requireActivity())
         bottomBinding = LayoutApplyBinding.inflate(layoutInflater)
         bottomSheetDialog.setContentView(bottomBinding.root)
 
+        userDb.getUser { b, user, s ->
+            if (b && user != null) {
+                bottomBinding.textUserName.text = "Dear ${user.name}"
+                bottomBinding.btnSend.setOnClickListener {
+                    val message =
+                        "Applicant has applied here are the details check whether the applicant is qualified to join you or not." +
+                                "\n\n*Applicant Details:*\n\t\t*Name:*\t${user.name}\n\t\t*Mobile No:*\t${user.phNo}\n\t\t*Email ID:*\t${user.email}\n\t\t*Resume URL:* ${user.resume}"
+                    val uri = Uri.parse("http://api.whatsapp.com/send?phone=+91$hrNo&text=${Uri.encode(message)}")
+                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                    startActivity(intent)
+                    dbModel.appliedJob(jobId){success,error->
+                        if (success){
+                            Toast.makeText(context, "Thank you for applied. You wait for replayed", Toast.LENGTH_SHORT).show()
+                        }else{
+                            Toast.makeText(context, "Error: $error", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                    bottomSheetDialog.dismiss()
+                }
+            }
+        }
+
         bottomSheetDialog.show()
     }
+
+
 }
